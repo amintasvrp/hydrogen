64901a2c5e0c5bd29c70b8a1f170a30b6a675f2a
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2021-06-21 01:09:13 -0300

    Multinodes + Consensus

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,14 @@
 import json
 import hashlib
+import requests
 from time import time
 from uuid import uuid4
+from urllib.parse import urlparse
 
 from textwrap import dedent
 from flask import Flask, jsonify, request
 from werkzeug.wrappers import response
+
 
 '''
 BLOCKCHAIN SERVICE
@@ -24,6 +27,21 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
+
+    def register_node(self, address):
+        '''
+        Add a new node to the set of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: <str> Node netloc (IP:Port)
+        '''
+
+        parsed_url = urlparse(address)
+        netloc = parsed_url.netloc
+        if netloc != "":
+            self.nodes.add(netloc)
+
+        return parsed_url.netloc
 
     def new_block(self, proof, previous_hash=None):
         '''
@@ -78,6 +96,65 @@ class Blockchain(object):
 
         return proof
 
+    def valid_chain(self, chain):
+        '''
+        Validate chain
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        '''
+        previous_index = 0
+        current_index = 1
+        chain_size = len(self.chain)
+        while current_index < chain_size:
+            previous_block = chain[previous_index]
+            current_block = chain[current_index]
+
+            # Check if the hash of the block is correct
+            if current_block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            # Check if the proof of work is correct
+            if not self.valid_proof(previous_block['proof'], current_block['proof']):
+                return False
+
+            previous_index += 1
+            current_index += 1
+
+        return True
+
+    def resolve_conficts(self):
+        '''
+        This is our Consensus Algorithm. It resolves conficts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        '''
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # Searching for chains longer than ours
+        max_length = len(self.chain)
+
+        # Get and check the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the chain is longer and valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we realise a new, valid and longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
     @property
     def last_block(self):
         if len(self.chain):
@@ -128,7 +205,7 @@ blockchain = Blockchain()
 def mine():
     if not len(blockchain.current_transactions):
         response = {
-            'message': 'No transactions found.'
+            'message': 'No transactions found'
         }
         return response, 404
 
@@ -198,6 +275,45 @@ def full_chain():
     return jsonify(response), 200
 
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values['nodes']
+    if not nodes:
+        response = {
+            'message': 'Invalid list of nodes'
+        }
+        return jsonify(response), 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conficts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
 # Run API
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
