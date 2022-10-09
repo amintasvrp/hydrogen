175f1f09006e46200a3783e1071f315b87fa4171
80851850 <amintas.v.pereira@accenture.com>
2022-10-09 15:28:37 -0300

    Fixing Issue #1 - Parallel the requests to the other nodes

--- src/providers/requester.py
+++ src/providers/requester.py
@@ -2,23 +2,30 @@
 Module responsible by http requests
 """
 
+import threading
+import concurrent.futures as executors
+
 import requests
 from providers.settings import get_rest_protocol
 from utils.constants import DEFAULT_DICT, ENDPOINT_CHAIN, STATUS_OK
 
 PROTOCOL = get_rest_protocol()
+results = []
+lock = threading.Lock()
 
 """
 Get chains from all nodes neighbours
 """
 
-
-def get_chains(nodes):
-    results = []
-    for node in nodes:
-        response = requests.get(f'{PROTOCOL}://{node}{ENDPOINT_CHAIN}')
+def get_chain(node):
+    response = requests.get(f'{PROTOCOL}://{node}{ENDPOINT_CHAIN}')
+    with lock:
         if response.status_code == STATUS_OK:
             results.append(response.json())
         else:
             results.append(DEFAULT_DICT)
+
+def get_chains(nodes):
+    with executors.ThreadPoolExecutor(max_workers=len(nodes)) as executor:
+        executor.map(get_chain, nodes)        
     return results
