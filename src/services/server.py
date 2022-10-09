6339f7ec46533839b16a272d2b44fd3c2fd830c2
80851850 <amintas.v.pereira@accenture.com>
2022-10-09 16:04:20 -0300

    Returning not mined transactions in /chain

--- src/services/server.py
+++ src/services/server.py
@@ -105,6 +105,7 @@ def new_transaction():
 @app.route(ENDPOINT_CHAIN, methods=['GET'])
 def full_chain():
     response = {
+        'transactions': blockchain.current_transactions,
         'chain': blockchain.chain,
         'length': len(blockchain.chain)
     }
